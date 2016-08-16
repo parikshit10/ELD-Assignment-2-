a07c85fa2a6f3769775664a8a11db75fb73b94d3
Parikshit <parikshit15155@iiitd.ac.in>
2016-08-16 23:47:14 +0530

    C file added

---
+++ blinkingled.c
@@ -0,0 +1,14 @@
+#define F_CPU 16000000L
+#include <avr/io.h>
+#include <util/delay.h>
+int main()
+{
+	DDRB |= _BV(DDB5);
+	while(1)
+	{
+		PORTB |= _BV(PORTB5);
+		_delay_ms(1000);
+		PORTB &= ~_BV(PORTB5);
+		_delay_ms(1000);
+	}
+}
